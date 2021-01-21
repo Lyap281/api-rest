fbc240440371c8c6e9d4ee8c44f3350dfca1beed
Léonie <lemaitre.leonie@laposte.net>
2021-01-21 17:17:16 +0100

    register.php completed

--- api/register.php
+++ api/register.php
@@ -3,6 +3,7 @@ $results = ["message"=>"", "error" => "" , "success" => true];
 
 $email = $_POST['email'];
 $password = sha1($_POST['password']);
+$verif = sha1($_POST['password2']);
 
 // Connexion à la BDD
 include_once 'db.php';
@@ -11,18 +12,43 @@ $db->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ERRMODE_WARNING);
 
 if(empty($email))
     {
-        $results['message'] = "veuillez indiquer une adresse email";
+        $results['message'] = "Veuillez indiquer une adresse email";
         $results['success'] = false;
     }
 
-if($results['success'])
+// if(empty($password))
+//     {
+//         $results['message'] = "Veuillez indiquer un mot de passe";
+//         $results['success'] = false;
+//     }
+
+if($password == $verif)
     {
-        $req = $db->prepare("INSERT INTO 'users' ('email', 'password') VALUES (:email, :password)");
-        $req->execute(array(email => $email, password => $password));
+        if($results['success'])
+            {
+                $request = $db->prepare("SELECT * FROM user WHERE email = ? ");
+                $request->execute(array($email));
+                $nbresult = $request->rowcount();
+
+                if($nbresult == 0)
+                {
+                    $req = $db->prepare("INSERT INTO user (email, password) VALUES (?, ?)");
+                    $req->execute(array($email, $password));
+                    $results['message'] = "Enregistrement effectué.";
+                }
+                else
+                {
+                    $results['message'] = "Vous êtes déjà enregistré.";
+                }
+            }
+    }
+    else
+    {
+        $results['message'] = "Les mots de passe ne sont pas identiques.";
     }
 
 
-echo json_encode(['message' => 'Vous êtes bien enregistré dans la base']);
+
 
 echo json_encode($results);
 
