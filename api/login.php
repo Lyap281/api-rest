e4dbd8baf0fa6aea8e49923cb41621e39dfe8553
Lyap281 <lemaitre.leonie@laposte.net>
2021-01-22 11:45:55 +0100

    try to login

--- api/login.php
+++ api/login.php
@@ -1,54 +1,54 @@
 <?php
-// $results = ["message"=>"", "error" => "" , "success" => true];
+$results = ["message"=>"", "error" => "" , "success" => true];
 
-// $email = $_POST['email'];
-// $password = sha1($_POST['password']);
+$email = $_POST['email'];
+$password = sha1($_POST['password']);
 
-// // Connexion à la BDD
-// include_once 'db.php';
+// Connexion à la BDD
+include_once 'db.php';
 
-// $db->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ERRMODE_WARNING);
+$db->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ERRMODE_WARNING);
 
-// if(empty($email))
-//     {
-//         $results['message'] = "Veuillez entrer une adresse email";
-//         $results['success'] = false;
-//     }
+if(empty($email))
+    {
+        $results['message'] = "Veuillez entrer une adresse email";
+        $results['success'] = false;
+    }
 
-// if(empty($password))
-//     {
-//         $results['message'] = "Veuillez entrer un mot de passe";
-//         $results['success'] = false;
-//     }
+if(empty($password))
+    {
+        $results['message'] = "Veuillez entrer un mot de passe";
+        $results['success'] = false;
+    }
 
 
 
-// if($results['success'])
-//     {
-//         $request = $db->prepare("SELECT * FROM user WHERE email = ? ");
-//         $request->execute(array($email));
-//         $nbresult = $request->rowcount();
+if($results['success'])
+    {
+        $request = $db->prepare("SELECT * FROM user WHERE email = ? ");
+        $request->execute(array($email));
+        $nbresult = $request->rowcount();
 
-//         if($nbresult == 1)
-//         {
-//             $req = $db->prepare("SELECT password FROM user WHERE email = ? ");
-//             $req->execute(array($email));
-//             $nbresults = $req->rowcount();
-//             if($nbresults == 1)
-//             {
+        if($nbresult == 1)
+        {
+            $req = $db->prepare("SELECT password FROM user WHERE email = ? ");
+            $req->execute(array($email));
+            $nbresults = $req->rowcount();
+            if($nbresults == 1)
+            {
                 
-//             }
-//             // $req = $db->prepare("INSERT INTO user (email, password) VALUES (?, ?)");
-//             // $req->execute(array($email, $password));
-//             // $results['message'] = "Enregistrement effectué.";
-//         }
-//         else
-//         {
-//             $results['message'] = "Vous êtes déjà enregistré.";
-//         }
-//     }
+            }
+            // $req = $db->prepare("INSERT INTO user (email, password) VALUES (?, ?)");
+            // $req->execute(array($email, $password));
+            // $results['message'] = "Enregistrement effectué.";
+        }
+        else
+        {
+            $results['message'] = "Vous êtes déjà enregistré.";
+        }
+    }
 
 
-// echo json_encode($results);
+echo json_encode($results);
 
 ?>
