55e2bc270770bea45c556647b87025729976bb8a
Jérémy Duhamel <professionnel@duhameljeremy.work>
2021-01-22 11:47:27 +0100

    Expérimentation lien front/api

--- forms/form_new.php
+++ forms/form_new.php
@@ -1,3 +1,22 @@
+<?php
+ //Trouver comment envoyer une requête POST avec le formulaire
+ /*Test d'un bout de code à remanier, trouvé ici : https://www.lije-creative.com/tuto-application-php-api-json/
+ /*if (isset($_POST['mot']) && !empty($_POST['mot'])) {
+    $motRecherche = urlencode(cleanString($_POST['mot']));
+    $dir = 'cache';
+    $match = '';
+    foreach (glob($dir . '/*.json') as $fichier) {
+        if (basename($fichier, '.json') == $motRecherche) {
+            $match = $fichier;
+        }
+    }
+}
+    
+$raw = file_get_contents($url);
+file_put_contents($dir . '/' . $motRecherche . '.json', $raw);
+$json = json_decode($raw);
+*/ 
+?>
 <h1>S'inscrire</h1>
 <div id="authentification">
     <form name="new" action="index.php" method="POST">
